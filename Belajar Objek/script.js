60e6a2989d45eececafe91ebbeeb66eaefba7610
Rahnad Suryadi <rahmadtkd13@gamil.com>
2022-09-18 19:42:09 +0700

    menambah cara2 membuat objek

--- Belajar Objek/script.js
+++ Belajar Objek/script.js
@@ -27,3 +27,39 @@ function Angkot(sopir, trayek, penumpang, kas) {
 
 var angkot1 = new Angkot("Ujang", ["kalianda", "bakauheni"], [], 0);
 var angkot2 = new Angkot("Udin", ["Sidomulyo", "katibung"], [], 0);
+
+//// membuat obejek
+
+//objek literal
+var mahs1 = {
+  nama: 'asda',
+  umur: 30
+}
+var mahs2 = {
+  nama: 'asda',
+  umur: 30
+}
+
+//func declaration
+
+function buatObjekMahasiswa(nama,umur){
+  var mhs ={}
+  mhs.nama = nama
+  mhs.umur = umur
+  return mhs
+}
+//cara pake harus panggil func
+var mah = buatObjekMahasiswa("iday",22)
+var mah1 = buatObjekMahasiswa("iday",22)
+
+
+///func constructor
+function MembuatConstructor(nama,umur){
+  // var this = {}
+  this.nama = nama
+  this.umur = umur
+  //return this
+}
+//cara jalanain
+const siswa2 = new MembuatConstructor("rahmad",22)
+const siswa3 = new MembuatConstructor("rahmad",22)
